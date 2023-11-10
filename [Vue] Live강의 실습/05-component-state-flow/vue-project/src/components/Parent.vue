a2328a7b46a9ef9bd83b149ee6a58bac9dc43717
sun <v00417@naver.com>
2023-11-10 17:54:50 +0900

    1110

---
+++ [Vue] Live강의 실습/05-component-state-flow/vue-project/src/components/Parent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ParentChild 
+      my-msg="message" 
+      :dynamic-props="name"
+      @some-event="someCallback"
+      @emit-args="getNumbers"
+      @update-name="updateName"
+    />
+  </div>
+</template>
+
+<script setup>
+import ParentChild from '@/components/ParentChild.vue'
+import { ref } from 'vue'
+
+const name = ref('Alice')
+
+const someCallback = function () {
+  console.log('ParentChild의 emit을 잘 받았습니다')
+}
+
+const getNumbers = function (...args) {
+  console.log(args)
+}
+
+const updateName = function () {
+  if (name.value === 'Alice') {
+    name.value = 'Bella'
+  } else {
+    name.value = 'Alice'
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
