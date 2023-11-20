b2bcfc573a9c21e7133f5fe80f309039bcbb6c3b
sun <v00417@naver.com>
2023-11-20 18:03:05 +0900

    1120

---
+++ [Vue] Live강의 실습/08-02-vue-with-drf/vue-project/src/views/LogInView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>LogIn</h1>
+    <form @submit.prevent="logIn">
+      <label>Username : </label>
+      <input type="text" v-model.trim="username"><br>
+      <label>Password : </label>
+      <input type="password" v-model.trim="password"><br>
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+const username = ref(null)
+const password = ref(null)
+
+const logIn = function () {
+  const payload = {
+    username: username.value,
+    password: password.value
+  }
+  store.logIn(payload)
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ [Vue] Live강의 실습/08-02-vue-with-drf/vue-project/src/views/SignUpView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>SignUp</h1>
+    <form @submit.prevent="signUp">
+      <label>Username : </label>
+      <input type="text" v-model.trim="username"><br>
+      <label>Password1 : </label>
+      <input type="password" v-model.trim="password1"><br>
+      <label>Password2 : </label>
+      <input type="password" v-model.trim="password2"><br>
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+const username = ref(null)
+const password1 = ref(null)
+const password2 = ref(null)
+
+const signUp = function () {
+  const payload = {
+    username: username.value,
+    password1: password1.value,
+    password2: password2.value,
+  }
+  store.signUp(payload)
+}
+</script>
+
+<style>
+
+</style>
